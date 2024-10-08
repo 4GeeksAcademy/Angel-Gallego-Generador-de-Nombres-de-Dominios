b47c75ff4b1b1a134cd8fad6acbe10ebbf6ebca8
Angel Gallego <angelgb1990@gmail.com>
2024-10-08 14:49:02 +0000

    java base ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const genBoton = document.getElementById("genBoton");
+// let imput = document.getElementById("Nick").value;
+let nick = "";
+let pre = ["el", "tu", "vuestro"];
+let mid = ["aterrador", "amante", "programador"];
+let fin = ["viciado", "escoces", "despistado"];
+let dominio = ["com", "es", "ado"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = function genDomin() {
+function introNick() {}
+function genDomin() {
+  const domGenerados = document.getElementById("domGenerados");
+  let dominiosGenerados = " ";
+  // let nick = prompt("¿Nick o nombre que te gustaria?");
+  for (let i = 0; i < pre.length; i++) {
+    for (let j = 0; j < mid.length; j++) {
+      for (let k = 0; k < fin.length; k++) {
+        for (let l = 0; l < dominio.length; l++) {
+          dominiosGenerados +=
+            pre[i] + mid[j] + fin[k] + "." + dominio[l] + " ";
+        }
+      }
+    }
+  }
+  domGenerados.innerHTML = dominiosGenerados;
+}
+genBoton.addEventListener("click", genDomin);
+genDomin();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// genBoton.addEventListener("click", genDomin);
+// genDomin();
+
+// for (let i = 0; i < pronoun.length; i++) {
+//   for (let j = 0; j < adj.length; j++) {
+//     for (let k = 0; k < noun.length; k++) {
+//       for (let l = 0; l < domain.length; l++) {
+//         let strDomainName = pronoun[i] + adj[j] + noun[k] + domain[l];
